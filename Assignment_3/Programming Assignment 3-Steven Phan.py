b278b8b98e4ce0b4a94c5c781417178a38143dce
Steven Phan <steven.phan009@gmail.com>
2019-11-01 20:22:50 -0700

    finished assignment 3

--- Assignment_3/Programming Assignment 3-Steven Phan.py
+++ Assignment_3/Programming Assignment 3-Steven Phan.py
@@ -1,4 +1,6 @@
 #Steven Phan
+#Shoraj Manandhar
+#Allen Lupisan
 
 #function takes in a list of lists
 #returns whether the gcd of all sublists is 1
@@ -37,7 +39,7 @@ def ChineseRemainderTheorem(L):
     M_list = list()
     Sum = 0
    
-   #check to see if relatie prime
+   #check to see if relative prime
     if (isRelativePrime(L)  == True):
         for group in L:
             a.append(group[0])
@@ -50,14 +52,12 @@ def ChineseRemainderTheorem(L):
             y.append(inverseY(M_list[times],m[times]))
             Sum += (a[times] * M_list[times]* y[times])
 
-        Sum = Sum % M
+        Sum = int(Sum % M)
 
         return Sum
 
     else:
-        string = "Cannot proceed, the modulus values are not relatively prime. " \
-                "Thus, you should also implement a relatively prime function "\
-                "to ensure the list of moduluses are relatively prime"
+        string = "Cannot proceed, the modulus values are not relatively prime. "
 
         return (string)
 
@@ -66,35 +66,10 @@ def main():
 
     # test = ([[2,3],[3,5],[2,7]])
     # test = ([[1,5], [2,7], [3,9], [4,11]])
-    test = ([1,5],[2,7], [3,9], [4,11],[8,22])
+    # test = ([1,5],[2,7], [3,9], [4,11],[8,22])
 
-    print(ChineseRemainderTheorem(test))
-
-    # a = list()
-    # m = list()
-    # y = list()
-    # count = 0
-    # M = 1
-    # M_list = list()
-    # Sum = 0
-
-
-    # for group in test2:
-    #     a.append(group[0])
-    #     m.append(group[1])
-    #     M *= group[1]
-    #     count+=1 
-
-    # for times in range(len(m)):
-    #     M_list.append(M/m[times])
-    #     y.append(inverseY(M_list[times],m[times]))
-    #     Sum += (a[times] * M_list[times]* y[times])
-
-    # Sum = Sum % M
-
-    # print(int(Sum))
-    # print(isRelativePrime(test3))
+    # print(ChineseRemainderTheorem(test))
+    pass
     
-
 if __name__ == "__main__": 
     main()
